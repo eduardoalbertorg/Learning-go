221e61872ae95f113f4efaaf161751c57f7a77e9
Eduardo Alberto Rodriguez Garcia <eduardoalbertorg@gmail.com>
2020-06-11 18:43:49 -0500

    Create hello world sample

---
+++ src/001_hello_world/hello_world.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+	foo()
+	fmt.Println("another print")
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
